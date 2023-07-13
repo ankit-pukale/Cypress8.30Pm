32b3e1c7739eb0d9f340f0bce9f24eb7634048ea
ankit-pukale <ankitpukale@gmail.com>
2023-07-13 21:28:37 +0530

    x path day 2

--- cypress/e2e/3xpath/testXpath.cy.js
+++ cypress/e2e/3xpath/testXpath.cy.js
@@ -14,4 +14,23 @@ describe('X path ', () => {
         //selecting third element using x path (Note ==> for xpath index start from 1)
         cy.xpath('(//div[@class="_2Brcj4"])[3]').should('contain','CONSUMER POLICY')
     })
+
+    ////*[@aria-label="Home & Furniture"]  use star to select attr and value with any tagName
+    it('Traverse towards parent element',()=>{
+        //to traverse towards parent el /..
+        cy.visit('https://www.flipkart.com/')
+        cy.xpath('//*[@aria-label="Home & Furniture"]/..').should('have.class','_3sdu8W')
+    })
+
+    it('traverse towards children el',()=>{
+        cy.visit('https://www.flipkart.com/')
+        //To traverse towards children use //tagname
+        cy.xpath('(//*[@aria-label="Home & Furniture"]//div//div)[1]').should('have.class','YBLJE4')
+        cy.xpath('//*[@aria-label="Home & Furniture"]//span[@class="_1XjE3T"]').should('have.class','_1XjE3T')
+    })
+
+    it('dynamic Xpath',()=>{
+        cy.visit('https://www.flipkart.com/')
+        cy.xpath('//div[@aria-label="$var"]'.replace('$var','Appliances'))
+    })
 });
